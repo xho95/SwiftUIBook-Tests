3c25e00f2558c40cde28dd0f696ed2170ea98af7
Kim, Min Ho <xho1995@gmail.com>
2022-06-08 16:40:12 +0900

    add an app to show Hello, world q quit

---
+++ HelloWorld/Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Min Ho Kim on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
