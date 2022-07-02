06d06a7f47d74ce2166faaaaf87ceae21a721c0b
Kim, Min Ho <xho1995@gmail.com>
2022-07-02 19:44:15 +0900

    add a new Hello project to capture some screenshots

---
+++ HelloWorld/Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Min Ho Kim on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
